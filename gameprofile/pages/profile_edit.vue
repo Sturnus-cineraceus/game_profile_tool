4b598de5120518b06b610a812f6f682c9b4239ad
SATOSHI <bigbrother926@gmail.com>
2022-01-27 22:38:15 +0900

    選択肢を増やした

--- gameprofile/pages/profile_edit.vue
+++ gameprofile/pages/profile_edit.vue
@@ -1,14 +1,236 @@
 <template>
   <div>
-    <b-form-input v-model="epic_name"></b-form-input>
-    <b-button variant="outline-primary" @click="click">Button</b-button>
+    <Header></Header>
+    <div class="contents">
+      <b-card-group deck class="basic_profile">
+        <b-card title="基本プロフィール">
+          <!-- <b-card-text>Header and footers using props.</b-card-text> -->
+          <b-form-group
+            id="epic_name_group"
+            label="EpicID:"
+            label-for="input-2"
+          >
+            <b-form-input
+              id="input-2"
+              v-model="form.name"
+              placeholder="Enter name"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-2"
+            label="チーム名:"
+            label-for="input-2"
+          >
+            <b-form-input
+              id="input-2"
+              v-model="form.name"
+              placeholder="Enter name"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-2" label="性別" label-for="input-2">
+            <b-form-select
+              v-model="form.sex"
+              :options="sex_opt"
+            ></b-form-select>
+          </b-form-group>
+
+          <b-form-group id="input-group-2" label="強さ" label-for="input-2">
+            <b-form-select
+              v-model="form.strength"
+              :options="strength_opt"
+            ></b-form-select>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-2"
+            label="よく遊んでるモード"
+            label-for="input-2"
+          >
+            <b-form-select
+              v-model="form.play"
+              :options="play_opt"
+            ></b-form-select>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-2"
+            label="ゲームで目指したいこと"
+            label-for="input-2"
+          >
+            <b-form-select
+              v-model="form.important"
+              :options="important_opt"
+            ></b-form-select>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-2"
+            label="遊んでる時間"
+            label-for="input-2"
+          >
+            <b-form-select
+              v-model="form.time"
+              :options="time_opt"
+            ></b-form-select>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-2"
+            label="コントローラー"
+            label-for="input-2"
+          >
+            <b-form-select
+              v-model="form.ctrler"
+              :options="ctrler_opt"
+            ></b-form-select>
+          </b-form-group>
+
+          <b-form-group id="input-group-2" label="デバイス" label-for="input-2">
+            <b-form-select
+              v-model="form.device"
+              :options="device_opt"
+            ></b-form-select>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-2"
+            label="ボイスチャット"
+            label-for="input-2"
+          >
+            <b-form-select v-model="form.vc" :options="vc_opt"></b-form-select>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-2"
+            label="Epicネーム:"
+            label-for="input-2"
+          >
+            <b-form-input
+              id="input-2"
+              v-model="form.name"
+              placeholder="Enter name"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-2"
+            label="Epicネーム:"
+            label-for="input-2"
+          >
+            <b-form-input
+              id="input-2"
+              v-model="form.name"
+              placeholder="Enter name"
+              required
+            ></b-form-input>
+          </b-form-group>
+        </b-card>
+      </b-card-group>
+    </div>
   </div>
 </template>
 <script>
 import axios from "axios";
+import Header from "@/components/header";
 export default {
+  components: { Header },
   name: "ProfileEdit",
-  data: () => ({ hoge: "das", epic_name: "" }),
+  data: () => ({
+    hoge: "das",
+    epic_name: "",
+    form: {
+      name: "",
+      sex: null,
+      strength: null,
+      play: null,
+      important: null,
+      vc: null,
+      time: null,
+      device: null,
+      ctrler: null,
+    },
+    sex_opt: [
+      { value: "n", text: "" },
+      { value: "m", text: "男性" },
+      { value: "f", text: "女性" },
+      { value: "x", text: "その他" },
+    ],
+    strength_opt: [
+      { value: "0", text: "" },
+      { value: "1", text: "1 始めたばかり" },
+      { value: "2", text: "2 初キル済み" },
+      { value: "3", text: "3 壁や階段は出せる" },
+      { value: "4", text: "4 ソロでビクロイはした" },
+      { value: "5", text: "5 建築バトルできる" },
+      { value: "6", text: "6 編集活用してバトルできる" },
+      { value: "7", text: "7 10キル以上余裕" },
+      { value: "8", text: "8 チャンピオンリーグ常連" },
+      { value: "9", text: "9 競技に本気" },
+      { value: "10", text: "10 プロとして生計を立てている" },
+    ],
+    play_opt: [
+      { value: "0", text: "" },
+      { value: "1", text: "ソロ" },
+      { value: "2", text: "デュオ" },
+      { value: "3", text: "トリオ" },
+      { value: "4", text: "スクワッド" },
+      { value: "5", text: "アリーナ ソロ" },
+      { value: "6", text: "アリーナ デュオ" },
+      { value: "7", text: "アリーナ トリオ" },
+      { value: "8", text: "チームランブル" },
+      { value: "9", text: "クリエイティブ" },
+      { value: "10", text: "スクリム" },
+      { value: "11", text: "その他" },
+    ],
+    important_opt: [
+      { value: "0", text: "" },
+      { value: "1", text: "みんなと協力してバトルしたい" },
+      { value: "2", text: "おしゃべりしたい ロビーにいるだけでもいい！" },
+      { value: "3", text: "ビクロイしたい" },
+      { value: "4", text: "たくさんキルしたい" },
+      { value: "5", text: "珍しい戦い方を検証したい" },
+      { value: "6", text: "フォートナイト考察のために島を回りたい" },
+      { value: "7", text: "クエストをクリアしたい" },
+      { value: "8", text: "動画映えするプレイを極めたい" },
+      { value: "9", text: "アリーナポイントためたい" },
+      { value: "10", text: "大会で良い成績を残したい" },
+    ],
+    vc_opt: [
+      { value: "0", text: "" },
+      { value: "1", text: "VCできません" },
+      { value: "2", text: "たまにできます" },
+      { value: "3", text: "いつでもオンです" },
+      { value: "4", text: "たまにできません" },
+      { value: "5", text: "仲良くなったら" },
+    ],
+    time_opt: [
+      { value: "0", text: "" },
+      { value: "1", text: "早朝" },
+      { value: "2", text: "日中帯" },
+      { value: "3", text: "夕方" },
+      { value: "4", text: "夜" },
+      { value: "5", text: "深夜" },
+    ],
+    ctrler_opt: [
+      { value: "0", text: "" },
+      { value: "1", text: "パッド" },
+      { value: "2", text: "キーマウ" },
+    ],
+    device_opt: [
+      { value: "0", text: "" },
+      { value: "1", text: "Switch" },
+      { value: "2", text: "PlayStation4" },
+      { value: "3", text: "PlayStation5" },
+      { value: "4", text: "XBox" },
+      { value: "5", text: "PC" },
+      { value: "6", text: "モバイル" },
+    ],
+  }),
   async mounted() {
     this.$authUtil.permitLogin().then(() => {
       // this.user = this.$store.state.user.data;
@@ -38,3 +260,14 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+div.contents {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .basic_profile {
+    padding-top: 5em;
+    width: 80%;
+  }
+}
+</style>
