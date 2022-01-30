43e3c00ab32691957e906269aa1741a9b0f9deab
SATOSHI <bigbrother926@gmail.com>
2022-01-30 14:13:16 +0900

    プロフィール登録

--- gameprofile/pages/logout.vue
+++ gameprofile/pages/logout.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="container has-text-centered mt-6">
     <div>
-      <h1 class="title">sample-nuxt-auth</h1>
       <h2 class="subtitle">ログアウトしました</h2>
     </div>
   </div>
 </template>
+<script>
+export default {
+  mounted() {},
+};
+</script>
