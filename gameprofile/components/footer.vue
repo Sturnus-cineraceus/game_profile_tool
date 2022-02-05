98d49e2dcc3074207ddca6269417ad5b42c44ac9
SATOSHI <bigbrother926@gmail.com>
2022-02-05 17:17:15 +0900

    ポリシー追加

--- gameprofile/components/footer.vue
+++ gameprofile/components/footer.vue
@@ -1,7 +1,32 @@
 <template>
   <div>
     <footer class="fixed-bottom">
-      <p>Copyright 2022 glorificatio.</p>
+      <b-row align-h="start">
+        <b-col cols="6" md="3">
+          <div>Copyright 2022 glorificatio.</div>
+        </b-col>
+        <b-col cols="4" md="3">
+          <div>
+            <nuxt-link to="/info/readme">各ポリシー</nuxt-link>
+          </div>
+        </b-col>
+      </b-row>
     </footer>
   </div>
 </template>
+<style lang="scss">
+footer {
+  // padding-top: 2em;
+  background-color: var(--cyan);
+  color: var(--white);
+  min-height: 1em;
+  div {
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+    padding-left: 0.5em;
+    a {
+      color: var(--white);
+    }
+  }
+}
+</style>
