a044e295490beff1e87c8f978afab5d20855c055
SATOSHI <bigbrother926@gmail.com>
2022-02-05 23:38:13 +0900

    名前間違い修正

--- gameprofile/pages/profile/_user_id/index.vue
+++ gameprofile/pages/profile/_user_id/index.vue
@@ -149,7 +149,7 @@ export default {
         { hid: 'description', name: 'description', content: this.profile_data.twitter_name + "さんのプロフ" },
         { hid: 'twitter:title', name: 'twitter:title', content: this.profile_data.twitter_name + "さんのフォトナプロフ" },
         { hid: 'twitter:description', name: 'twitter:description', content: desc },
-        { hid: 'twitter:card', name: 'twitter:card', content: 'Large-Summary-image' },
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
         { hid: 'twitter:description', name: 'twitter:description', content: desc },
         { hid: 'og:url', property: 'og:url', content: nowurl },
         { hid: 'og:image', property: 'og:image', content: ogimg },
